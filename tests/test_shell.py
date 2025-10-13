14dd6140cf20b139511b1a0526b89b28039fb953
Marius Gerdes <integr@gmail.com>
2025-10-14 00:33:13 +0200

    Add tests for shell module.

---
+++ tests/test_shell.py
@@ -0,0 +1,15 @@
+import unittest
+import json
+from typing import *
+from ghostcode.shell import *
+
+
+class VirtualTerminalTest(unittest.TestCase):
+    # Fill this in
+    pass
+def main():
+    unittest.main()
+
+    
+if __name__ == "__main__":
+    main()
